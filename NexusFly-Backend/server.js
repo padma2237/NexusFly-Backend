38975e8e3d846cb611234115ee8d0e9ecc2439e0
Padmaram Pegu <padmarampegu2237@gmail.com>
2026-07-14 09:45:35 +0530

    test gemini 2

--- NexusFly-Backend/server.js
+++ NexusFly-Backend/server.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 const app = express();
 const {
   searchWeb
@@ -10,7 +13,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('Public'));
 
-const path = require('path');
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.5-flash",
+});
+
+
 
 // Update your main route to send the index.html file
 app.get('/', (req, res) => {
@@ -63,32 +72,14 @@ app.post('/ask', async (req, res) => {
       };
     }
 
-    const requestBody = {
-      system_instruction: {
-        parts: [{
-          text: "You are NexusFly, a creative and friendly assistant. Never introduce yourself repeatedly. Answer the user's questions directly and creatively."
-        }]
-      },
-      contents: finalContents
-    };
+const result = await model.generateContent({
+  systemInstruction:
+    "You are NexusFly, a creative and friendly assistant. Never introduce yourself repeatedly. Answer the user's questions directly and creatively.",
+  contents: finalContents,
+});
 
-
-    const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-3.1-flash-lite:generateContent?key=${process.env.API_KEY}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(requestBody),
-      }
-    );
-
-    const data = await response.json();
-
-    const answer =
-    data?.candidates?.[0]?.content?.parts?.[0]?.text ??
-    "No response.";
+const answer = result.response.text();
+  
 
     const sources =
     webSearch && searchResults?.results
