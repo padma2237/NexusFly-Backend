1be14455da223175983ee71b0e8a28ff4e740cf0
Padmaram Pegu <padmarampegu2237@gmail.com>
2026-08-24 20:03:19 +0530

    Increase JSON body limit for image uploads

--- NexusFly-Backend/server.js
+++ NexusFly-Backend/server.js
@@ -9,9 +9,15 @@ const {
   searchWeb
 } = require("./services/webSearch");
 
+
+
 app.use(cors());
-app.use(express.json());
-app.use(express.static('Public'));
+
+app.use(express.json({
+  limit: "10mb",
+}));
+
+app.use(express.static("Public"));
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
